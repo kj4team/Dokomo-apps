630e0664c6d34e403d3d63e6bf6fcfeb0f4f509a
Kal Hoang <kal@kal.vn>
2023-04-30 17:27:58 +0700

    Fix ZOA

--- recipes/zalo-oa/webview.js
+++ recipes/zalo-oa/webview.js
@@ -1,20 +1,8 @@
-const _path = _interopRequireDefault(require('path'));
-
-function _interopRequireDefault(obj) {
-  return obj && obj.__esModule ? obj : { default: obj };
-}
-
 module.exports = Dokomo => {
   const getMessages = () => {
-    let count = 0;
-
-    const queryList = document.querySelectorAll('#unreadConvs');
-    for (const element of queryList) {
-      count += Dokomo.safeParseInt(element.textContent);
-    }
-    Dokomo.setBadge(count);
+    const notificationBadge = document.querySelectorAll('.tab-red-dot').length;
+    Dokomo.setBadge(notificationBadge);
   };
-  Dokomo.loop(getMessages);
 
-  // Dokomo.injectCSS(_path.default.join(__dirname, 'service.css'));
+  Dokomo.loop(getMessages);
 };
