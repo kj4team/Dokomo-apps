0bda044a84b5b8caa910f33e3d3205a04bef9f0e
Kal@Mac <kal@kal.vn>
2023-10-09 11:52:39 +0700

    Fix Zalo OA

--- recipes/zalo-oa/webview.js
+++ recipes/zalo-oa/webview.js
@@ -6,7 +6,9 @@ const _path = _interopRequireDefault(require('path'));
 
 module.exports = Dokomo => {
   const getMessages = () => {
-    const notificationBadge = document.querySelectorAll('.tab-red-dot').length;
+    // const notificationBadge = document.querySelectorAll('.tab-red-dot').length;
+    const notificationBadge =
+      document.querySelector('#unreadConvs').textContent;
     Dokomo.setBadge(notificationBadge);
   };
 
